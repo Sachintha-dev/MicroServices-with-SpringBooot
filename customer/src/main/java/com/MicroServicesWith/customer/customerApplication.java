e00f9bf1f51c6479de98a000c4c72749b0cb301f
sachintha <sudeshsachintha2016@gmail.com>
2022-08-25 14:47:49 +0530

    done:create a customer microservices

---
+++ customer/src/main/java/com/MicroServicesWith/customer/customerApplication.java
@@ -0,0 +1,13 @@
+package com.MicroServicesWith.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class customerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(customerApplication.class,args);
+    }
+}
